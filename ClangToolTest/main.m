e38a5825e6fbd9a9e9233eb5afbae96a2fdc26ab
jingsong.jn <jingsong.jn@alibaba-inc.com>
2017-02-28 17:00:32 +0800

    Initial Commit

---
+++ ClangToolTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ClangToolTest
+//
+//  Created by jingsong.jn on 2017/2/28.
+//  Copyright © 2017年 jingsong.jn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
